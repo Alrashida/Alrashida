2ff534bdf3e021433f07d59baf62eaf72a84bda8
Alrashida <97579480+alrashida@users.noreply.github.com>
2022-01-12 13:47:37 +0700

    Add files via upload

---
+++ Restoran.js
@@ -0,0 +1,9 @@
+let Restoran = {
+    name : "restaurant wanmin",
+    city : "liyue",
+    favfood : "jade parcel",
+    favdrink : "cocogoat",
+    isvegan :true,
+    "unyahhh" : "heeee",
+}
+console.log(Restoran)
---
+++ keluarga.js
@@ -0,0 +1,9 @@
+let keluarga = {
+    ayah : "bokap",
+    ibu : "nyokap",
+    kakak : 3,
+    ade : 1,
+    isjedi:true,
+    "home world": "buehehe",
+}
+console.log(keluarga)
